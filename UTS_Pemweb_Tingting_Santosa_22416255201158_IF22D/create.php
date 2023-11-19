2bb2ca0bf39230514dd2af0f6bd2fa7a2f188dea
TingtingSantosa <151390112+TingtingSantosa@users.noreply.github.com>
2023-11-20 01:02:44 +0700

    Add files via upload

---
+++ UTS_Pemweb_Tingting_Santosa_22416255201158_IF22D/create.php
@@ -0,0 +1,20 @@
+<!-- create.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Produk</title>
+</head>
+<body>
+    
+    <form action="./backend/create.php" method="post" enctype="multipart/form-data">
+        <h1>Tambah Produk</h1>
+        <input type="text" name="name" placeholder="input nama produk">
+        <input type="number" name="price" placeholder="input harga produk">
+        <input type="file" name="image">
+        <input type="submit" value="simpan" name="submit">
+        <a href="show.php">Lihat data produk</a>
+    </form>
+</body>
+</html>
